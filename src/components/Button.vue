6ddfb9c06ce70fc332af6aee5d07b6a09cc5c67f
Avisha Kulkarni <avisha.kulkarni@octech.in>
2024-03-12 15:44:01 +0530

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+<!-- v-on:click="onClick()" 
+or
+@click-->
+    <button @click="onClick()" :style="{ background: color}"
+     class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+       text: String,
+       color: String, 
+    },
+    methods: {
+        onClick() {
+            console.log("click")
+        },
+    },
+}
+</script>
